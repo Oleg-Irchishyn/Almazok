31305ab902ee1b21838956626d5e50bb87a0db49
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-08-27 11:50:53 +0300

    redesigning project

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// Entrypoint (For imports only)
+import 'bootstrap';
+import 'lazysizes';
+import './components/libs/jquery-validation-1.14.0/jquery.validate';
+import './components/libs/fancybox/dist/jquery.fancybox.min';
+import './components/libs/slick-carousel/slick/slick.min';
+import './components/common';
