9b75aaebc56833faf5ac8ad0bed05d84ba539eaf
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-08-28 16:49:26 +0300

    updating project

--- src/js/components/common.js
+++ src/js/components/common.js
@@ -136,6 +136,9 @@ $(document).ready(function () {
         $('.scroll_to').click(function (e) {
             var jump = $(this).attr('href');
             var new_position = $(jump).offset();
+            HeaderBurgerMenu.toggleClass("change"); 
+            Header.toggle();
+            Header.toggleClass('displayflex');
             $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
             e.preventDefault();
         });
